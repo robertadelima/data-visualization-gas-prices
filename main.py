0252d80e1132f45d1c85c3a3002738e49c02b5db
Marcel <dolly.marcel@gmail.com>
2020-06-25 23:09:00 -0300

    Organize main.py file

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import pandas as pd
-import plotly.express as px 
+import plotly.express as px
 import plotly.graph_objects as go
 
-import dash 
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+def clean_data_and_generate_dataset():
+    # Import data
+    df1 = pd.read_csv("dados-ANP-2013-2020.csv", sep=';', encoding='cp1252')
+    df2 = pd.read_csv("dados-IBGE-municipios.csv", sep=';', encoding='cp1252')
+    # Removing accents
+    df2['NOME MUNICIPIO'] = df2['NOME MUNICIPIO'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
+    # IBGE data to upper case
+    df2['NOME MUNICIPIO'] = df2['NOME MUNICIPIO'].str.upper()
+    # Left join
+    df3 = pd.merge(df1, df2, left_on='MUNICÍPIO', right_on='NOME MUNICIPIO', how ='left') 
+    return df3
+
+df = clean_data_and_generate_dataset()
+print(df.head(3))
+
+# Generate the app
 app = dash.Dash(__name__)
-
-# Import data
-df1 = pd.read_csv("dados-ANP-2013-2020.csv", sep=';', encoding='cp1252')
-df2 = pd.read_csv("dados-IBGE-municipios.csv", sep=';', encoding='cp1252')
-# Removing accents
-df2['NOME MUNICIPIO'] = df2['NOME MUNICIPIO'].str.normalize('NFKD').str.encode('ascii', errors='ignore').str.decode('utf-8')
-# IBGE data to upper case
-df2['NOME MUNICIPIO'] = df2['NOME MUNICIPIO'].str.upper()
-# Left join
-df3 = pd.merge(df1, df2, left_on='MUNICÍPIO', right_on='NOME MUNICIPIO', how ='left') 
-print(df3.head())
-
-
-# App layout
 app.layout = html.Div([
 
     html.H1("Preços dos Combustíveis - Santa Catarina, Brasil", style={'text-align': 'center'}),
 
 ])
-
 
 # Run
 if __name__ == '__main__':
