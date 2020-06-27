133cb8e419d6ff27c26aba1583cc1a9b40969b9b
robertadelima <robertarl@gmail.com>
2020-06-27 12:00:28 -0300

    using class column names for

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from dash.dependencies import Input, Output
 from data_provider import *
 
 df = DATASET
-#print(df.head(3))
 
 app = dash.Dash(__name__)
 
@@ -24,7 +23,7 @@ def options_from_dict(dictionary):
 # --------------------
 
 cities_map = html.Div([
-    dcc.Dropdown(id="cidades_selecionadas",
+    dcc.Dropdown(id="selected_cities",
                  options=options_from_dict(CITIES),
                  multi=True,
                  value=1,
@@ -80,7 +79,7 @@ filters = html.Div([
         className="dcc_control"
     ),
     dcc.RadioItems(
-        id="selected_fuel_radio",
+        id="selected_product",
         options=options_from_dict(PRODUCTS),
         value=0,
         labelStyle={"display": "inline-block"},
@@ -134,18 +133,18 @@ app.layout = html.Div([
      Output(component_id='brazil_map', component_property='figure'),
      Output(component_id='market_price_mean_graph', component_property='figure')
      ],
-    [Input(component_id='cidades_selecionadas', component_property='value'),
-    Input(component_id='selected_fuel_radio', component_property='value')]
+    [Input(component_id='selected_cities', component_property='value'),
+    Input(component_id='selected_product', component_property='value')]
 )
     # Plotly Express
-def update_graph(cidades_selecionadas, selected_fuel_radio):
-    if not isinstance(cidades_selecionadas, list): cidades_selecionadas = [cidades_selecionadas]
-    nomes_cidades = [CITIES[int(cidade)] for cidade in cidades_selecionadas]
-    container = "Região selecionada: {} Produto selecionado: {}".format(nomes_cidades, selected_fuel_radio)
+def update_graph(selected_cities, selected_product):
+    if not isinstance(selected_cities, list): selected_cities = [selected_cities]
+    nomes_cidades = [CITIES[int(cidade)] for cidade in selected_cities]
+    container = "Região selecionada: {} Produto selecionado: {}".format(nomes_cidades, selected_product)
 
     dff = df.copy()
-    dff = dff[dff["NOME MUNICIPIO"].isin(nomes_cidades)]
-    dff = dff[dff["PRODUTO"] == PRODUCTS[int(selected_fuel_radio)]]
+    dff = dff[dff[COLUMNS.CITY_NAME].isin(nomes_cidades)]
+    dff = dff[dff[COLUMNS.PRODUCT] == PRODUCTS[int(selected_product)]]
 
     px.set_mapbox_access_token(open(".mapbox_token.txt").read())
     fig = px.scatter_mapbox(dff, lat=COLUMNS.LATITUDE, lon=COLUMNS.LONGITUDE, color_continuous_scale=px.colors.cyclical.IceFire, 
@@ -154,7 +153,7 @@ def update_graph(cidades_selecionadas, selected_fuel_radio):
     
     dfff = dff.groupby([COLUMNS.MONTH, COLUMNS.CITY])[COLUMNS.MARKET_PRICE_MEAN].apply(list)
 
-    fig2 = px.line(dff, x=COLUMNS.MONTH, y=COLUMNS.MARKET_PRICE_MEAN, line_group='MUNICÍPIO', color='MUNICÍPIO')
+    fig2 = px.line(dff, x=COLUMNS.MONTH, y=COLUMNS.MARKET_PRICE_MEAN, line_group=COLUMNS.CITY, color=COLUMNS.CITY)
     return container, fig , fig2
 
 # Run
